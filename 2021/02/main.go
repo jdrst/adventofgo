0afbcb21284a2de956f2501120ef01a78ae18d2e
jd <threeman@gmx.de>
2021-12-02 06:45:38 +0100

    adds 2021\02

---
+++ 2021/02/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"git.threeman.info/jd/adventofcode/util"
+)
+
+func main() {
+	fmt.Printf("First part: %v\n", partOne(util.ReadFile("input.txt")))
+	fmt.Printf("Second part: %v\n", partTwo(util.ReadFile("input.txt")))
+}
+
+func partOne(file util.File) int {
+	lines := file.AsLines()
+	hPos, depth := 0, 0
+	for _, l := range lines {
+		command := l.SubSplitWith(" ")
+		switch command[0] {
+		case "forward":
+			hPos += command[1].AsInt()
+		case "down":
+			depth += command[1].AsInt()
+		case "up":
+			depth -= command[1].AsInt()
+		default:
+			log.Fatalf("can't")
+		}
+	}
+	return hPos * depth
+}
+
+func partTwo(file util.File) int {
+	lines := file.AsLines()
+	hPos, depth, aim := 0, 0, 0
+	for _, l := range lines {
+		command := l.SubSplitWith(" ")
+		switch command[0] {
+		case "forward":
+			hPos += command[1].AsInt()
+			depth += aim * command[1].AsInt()
+		case "down":
+			aim += command[1].AsInt()
+		case "up":
+			aim -= command[1].AsInt()
+		default:
+			log.Fatalf("can't")
+		}
+	}
+	return hPos * depth
+}
