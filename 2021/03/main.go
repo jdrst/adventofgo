2700c77e3e10eb5c72a6e94cf9dcf21fdcdc1325
jd <threeman@gmx.de>
2021-12-03 08:41:38 +0100

    refactors part 1 and renames part 2

--- 2021/03/main.go
+++ 2021/03/main.go
@@ -14,45 +14,46 @@ func main() {
 
 func partOne(file util.File) int {
 	lines := file.AsLines()
-	mostCommonBits := make([]byte, len(lines[0]))
-	for i := range mostCommonBits {
+	gammaRate := 0
+	for i := 0; i < len(lines[0]); i++ {
+		gammaRate <<= 1
 		if getMostCommonBitOnPos(i, lines) {
-			mostCommonBits[i] = '1'
-		} else {
-			mostCommonBits[i] = '0'
+			gammaRate |= 1
 		}
 	}
-	gammaRate, err := strconv.ParseInt(string(mostCommonBits), 2, 64)
-	util.Handle(err)
 	return int(gammaRate * (1<<len(lines[0]) - 1 ^ gammaRate))
 }
 
 func partTwo(file util.File) int {
-	lines := file.AsLines()
-	lines2 := file.AsLines()
-	for i := 0; i < len(lines[0]); i++ {
-		mostCommon := getMostCommonBitOnPos(i, lines)
-		for j := 0; j < len(lines) && len(lines) > 1; j++ {
-			if string(lines[j][i]) == "1" == mostCommon {
+	oxy := file.AsLines()
+	co2 := file.AsLines()
+
+	for i := 0; i < len(oxy[0]); i++ {
+		mostCommon := getMostCommonBitOnPos(i, oxy)
+		leastCommon := !getMostCommonBitOnPos(i, co2)
+
+		for j := 0; j < len(oxy) && len(oxy) > 1; j++ {
+			if string(oxy[j][i]) == "1" == mostCommon {
 				continue
 			}
-			lines[j] = lines[len(lines)-1]
-			lines = lines[:len(lines)-1]
+			oxy[j] = oxy[len(oxy)-1]
+			oxy = oxy[:len(oxy)-1]
 			j--
 		}
-		leastCommon := getLeastCommonBitOnPos(i, lines2)
-		for j := 0; j < len(lines2) && len(lines2) > 1; j++ {
-			if string(lines2[j][i]) == "1" == leastCommon {
+
+		for j := 0; j < len(co2) && len(co2) > 1; j++ {
+			if string(co2[j][i]) == "1" == leastCommon {
 				continue
 			}
-			lines2[j] = lines2[len(lines2)-1]
-			lines2 = lines2[:len(lines2)-1]
+			co2[j] = co2[len(co2)-1]
+			co2 = co2[:len(co2)-1]
 			j--
 		}
 	}
-	first, err := strconv.ParseInt(string(lines[0]), 2, 64)
+
+	first, err := strconv.ParseInt(string(oxy[0]), 2, 64)
 	util.Handle(err)
-	second, err := strconv.ParseInt(string(lines2[0]), 2, 64)
+	second, err := strconv.ParseInt(string(co2[0]), 2, 64)
 	util.Handle(err)
 	return int(first * second)
 }
@@ -65,14 +66,4 @@ func getMostCommonBitOnPos(pos int, lines util.Lines) bool {
 		}
 	}
 	return float64(cnt) >= float64(len(lines))/2
-}
-
-func getLeastCommonBitOnPos(pos int, lines util.Lines) bool {
-	cnt := 0
-	for _, l := range lines {
-		if l[pos] == '1' {
-			cnt++
-		}
-	}
-	return float64(cnt) < float64(len(lines))/2
 }
