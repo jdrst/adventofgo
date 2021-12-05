a1a1d31f3a8c38f9220719d8ab2045bca2fe7daa
jd <75489664+jdrst@users.noreply.github.com>
2021-12-05 08:32:25 +0100

    applies some DRY and cleanup

--- 2021/05/main.go
+++ 2021/05/main.go
@@ -6,6 +6,8 @@ import (
 	"github.com/jdrst/adventofgo/util"
 )
 
+type VentLocations map[Point]int
+
 type Point struct {
 	x, y int
 }
@@ -16,11 +18,19 @@ func main() {
 }
 
 func partOne(file util.File) int {
+	return GetOverlapping(file, func(p1, p2 Point) bool { return p1.x == p2.x || p1.y == p2.y })
+}
+
+func partTwo(file util.File) int {
+	return GetOverlapping(file, func(p1, p2 Point) bool { return true })
+}
+
+func GetOverlapping(file util.File, constraintFunc func(Point, Point) bool) int {
 	lines := file.AsLines()
 
-	vents := make([][]Point, len(lines))
+	vents := make(VentLocations)
 
-	for i, l := range lines {
+	for _, l := range lines {
 		pAS := l.SubSplitWith(" -> ")
 
 		p1s := pAS[0].SubSplitWith(",")
@@ -29,29 +39,15 @@ func partOne(file util.File) int {
 		p2s := pAS[1].SubSplitWith(",")
 		p2 := Point{p2s[0].AsInt(), p2s[1].AsInt()}
 
-		if p1.x == p2.x || p1.y == p2.y {
-			vents[i] = createVents(p1, p2)
+		if constraintFunc(p1, p2) {
+			vents.AddVent(p1, p2)
 		}
 	}
-	ventMap := createVentMap(vents)
-	return countOverlappingPoints(ventMap)
+	return vents.OverlappingCount()
 }
 
-func createVentMap(pts [][]Point) map[Point]int {
-	ventMap := make(map[Point]int)
-	for _, vts := range pts {
-		for _, p := range vts {
-			if _, exists := ventMap[p]; !exists {
-				ventMap[p] = 0
-			}
-			ventMap[p]++
-		}
-	}
-	return ventMap
-}
-
-func countOverlappingPoints(ventMap map[Point]int) (sum int) {
-	for _, v := range ventMap {
+func (loc VentLocations) OverlappingCount() (sum int) {
+	for _, v := range loc {
 		if v > 1 {
 			sum++
 		}
@@ -59,50 +55,33 @@ func countOverlappingPoints(ventMap map[Point]int) (sum int) {
 	return
 }
 
-func createVents(start, end Point) []Point {
-	res := []Point{}
-	fromX, fromY, toX, toY := start.x, start.y, end.x, end.y
-
-	vec := Point{0, 0}
-	if fromX > toX {
-		vec.x = -1
+func (loc VentLocations) AddPoint(p Point) {
+	if _, exists := loc[p]; !exists {
+		loc[p] = 0
 	}
-	if fromX < toX {
-		vec.x = 1
-	}
-	if fromY > toY {
-		vec.y = -1
-	}
-	if fromY < toY {
-		vec.y = 1
-	}
-
-	for fromY != toY || fromX != toX {
-		res = append(res, Point{fromX, fromY})
-		fromX += vec.x
-		fromY += vec.y
-	}
-	res = append(res, Point{fromX, fromY})
-
-	return res
+	loc[p]++
 }
 
-func partTwo(file util.File) int {
-	lines := file.AsLines()
+func (loc VentLocations) AddVent(start, end Point) {
+	delta := Point{0, 0}
 
-	vents := make([][]Point, len(lines))
-
-	for i, l := range lines {
-		pAS := l.SubSplitWith(" -> ")
-
-		p1s := pAS[0].SubSplitWith(",")
-		p1 := Point{p1s[0].AsInt(), p1s[1].AsInt()}
-
-		p2s := pAS[1].SubSplitWith(",")
-		p2 := Point{p2s[0].AsInt(), p2s[1].AsInt()}
-
-		vents[i] = createVents(p1, p2)
+	if start.x > end.x {
+		delta.x = -1
 	}
-	ventMap := createVentMap(vents)
-	return countOverlappingPoints(ventMap)
+	if start.x < end.x {
+		delta.x = 1
+	}
+	if start.y > end.y {
+		delta.y = -1
+	}
+	if start.y < end.y {
+		delta.y = 1
+	}
+
+	for start.y != end.y || start.x != end.x {
+		loc.AddPoint(Point{start.x, start.y})
+		start.x += delta.x
+		start.y += delta.y
+	}
+	loc.AddPoint(Point{start.x, start.y})
 }
