7404f95f132c3427900ed9060f3ab60a02ad8049
jd <75489664+jdrst@users.noreply.github.com>
2021-12-06 09:49:32 +0100

    adds 2021/06 part 2

--- 2021/06/main.go
+++ 2021/06/main.go
@@ -6,35 +6,35 @@ import (
 	"github.com/jdrst/adventofgo/util"
 )
 
-type Lanternfish int
-
 func main() {
-	fmt.Printf("First part: %v\n", partOne(util.ReadFile("input.txt"), 80))
-	fmt.Printf("Second part: %v\n", partOne(util.ReadFile("input.txt"), 256))
+	fmt.Printf("First part: %v\n", lanternfishSpawnedAfter(80, util.ReadFile("input.txt")))
+	fmt.Printf("Second part: %v\n", lanternfishSpawnedAfter(256, util.ReadFile("input.txt")))
 }
 
-func partOne(file util.File, days int) int {
+func lanternfishSpawnedAfter(days int, file util.File) int {
 	lines := file.AsLines()
 	ages := lines[0].SubSplitWith(",").AsInts()
-	fish := make([]Lanternfish, len(ages))
-	for i, a := range ages {
-		fish[i] = Lanternfish(a)
-	}
-	for i := 0; i < days; i++ {
-		curFishes := len(fish)
-		for j := 0; j < curFishes; j++ {
-			if fish[j] == 0 {
-				new := Lanternfish(8)
-				fish = append(fish, new)
-				fish[j] = 7
-			}
-			fish[j]--
-		}
-	}
-	return len(fish)
-}
 
-func partTwo(file util.File) string {
-	lines := file.AsLines()
-	return string(lines[1])
+	birthingFish := [7]int{}
+	newBirthingFish := [9]int{}
+
+	for _, a := range ages {
+		birthingFish[a]++
+	}
+
+	for i := 0; i < days; i++ {
+		newBirthingFish[i%9], birthingFish[i%7] = birthingFish[i%7]+newBirthingFish[i%9], birthingFish[i%7]+newBirthingFish[i%9]
+	}
+
+	sum := 0
+
+	for _, n := range birthingFish {
+		sum += n
+	}
+
+	for _, n := range newBirthingFish {
+		sum += n
+	}
+
+	return sum
 }
--- 2021/06/main_test.go
+++ 2021/06/main_test.go
@@ -8,38 +8,23 @@ import (
 
 var testInput = `3,4,3,1,2`
 
-func TestPartOne(t *testing.T) {
+func TestLanternfishSpawnedAfter(t *testing.T) {
 	expected := 26
-	actual := partOne(util.File(testInput).WithOSLinebreaks(), 18)
+	actual := lanternfishSpawnedAfter(18, util.File(testInput).WithOSLinebreaks())
 	if actual != expected {
 		t.Errorf("expected was: %v \n actual is: %v", expected, actual)
 	}
 
 	expected = 5934
-	actual = partOne(util.File(testInput).WithOSLinebreaks(), 80)
+	actual = lanternfishSpawnedAfter(80, util.File(testInput).WithOSLinebreaks())
 	if actual != expected {
 		t.Errorf("expected was: %v \n actual is: %v", expected, actual)
 	}
 }
 
-// func TestPartTwo(t *testing.T) {
-// 	expected := "testTwo"
-// 	actual := partTwo(util.File(testInput).WithOSLinebreaks())
-// 	if actual != expected {
-// 		t.Errorf("\nexpected was: %v\nactual is: %v", expected, actual)
-// 	}
-// }
-
-func BenchmarkPartOne(b *testing.B) {
+func BenchmarkLanternfishSpawnedAfter(b *testing.B) {
 	input := util.ReadFile("input.txt")
 	for n := 0; n < b.N; n++ {
-		partOne(input, 80)
+		lanternfishSpawnedAfter(256, input)
 	}
 }
-
-// func BenchmarkPartTwo(b *testing.B) {
-// 	input := util.ReadFile("input.txt")
-// 	for n := 0; n < b.N; n++ {
-// 		partTwo(input)
-// 	}
-// }
