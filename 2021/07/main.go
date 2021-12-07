6373a488978223fd2debe3aff767f99f0a6718e9
jd <75489664+jdrst@users.noreply.github.com>
2021-12-07 07:36:05 +0100

    applies some dry

--- 2021/07/main.go
+++ 2021/07/main.go
@@ -13,6 +13,14 @@ func main() {
 }
 
 func partOne(file util.File) int {
+	return calculateMinimalTotalFuel(file, func(a, b int) int { return abs(a - b) })
+}
+
+func partTwo(file util.File) int {
+	return calculateMinimalTotalFuel(file, func(a, b int) int { return gaussianSum(abs(a - b - 1)) })
+}
+
+func calculateMinimalTotalFuel(file util.File, calcFunc func(int, int) int) int {
 	lines := file.AsLines()
 	crabs := lines[0].SubSplitWith(",").AsInts()
 
@@ -26,11 +34,14 @@ func partOne(file util.File) int {
 			min = c
 		}
 	}
+
 	totalFuels := make([]int, max-min)
 
 	for i := range totalFuels {
 		for _, c := range crabs {
-			totalFuels[i] += abs(c - i)
+			{
+				totalFuels[i] += calcFunc(i, c)
+			}
 		}
 	}
 
@@ -53,44 +64,4 @@ func abs(a int) int {
 
 func gaussianSum(to int) int {
 	return (to * (to + 1)) / 2
-}
-
-func partTwo(file util.File) int {
-	lines := file.AsLines()
-	crabs := lines[0].SubSplitWith(",").AsInts()
-
-	max := math.MinInt
-	min := math.MaxInt
-	for _, c := range crabs {
-		if c > max {
-			max = c
-		}
-		if c < min {
-			min = c
-		}
-	}
-	totalFuels := make([]int, max-min)
-
-	calcFuel := func(a, b int) int {
-		return gaussianSum(abs(a - b))
-	}
-
-	fmt.Println(calcFuel(16, 5))
-
-	for i := range totalFuels {
-		for _, c := range crabs {
-			{
-				totalFuels[i] += gaussianSum(abs(i - c - 1))
-			}
-		}
-	}
-
-	min = math.MaxInt
-	for _, f := range totalFuels {
-		if f < min {
-			min = f
-		}
-	}
-
-	return min
 }
